8222a39c9a97118dffda23430fbe1d4e2b36a797
wu576587441 <56558856+wu576587441@users.noreply.github.com>
2020-02-26 22:37:27 +0100

    Update feature selection.R

--- feature selection.R
+++ feature selection.R
@@ -65,3 +65,24 @@ for (i in 1:100) {
   
   top_features_GTV[i,] <-rfProfile_GTV$optVariables[1:8]
   AUCs_GTV[i] <- ForestRocTesting_GTV$auc
+
+#--features order
+top_features_GTV<-as.vector(top_features_GTV, mode="any")
+top_featuresfreq_GTV<-count(top_features_GTV)
+top_topfeaturesfreq_GTV<-top_featuresfreq_GTV[order(top_featuresfreq_GTV[,"freq"], decreasing= T), ]
+top_topfeaturesfreq_GTV
+
+#-- More than 30
+selected_30_GTV <- subset(top_topfeaturesfreq_GTV, freq >= 30,select = c(x))
+GTV_top_features_name<- array(selected_30_GTV$x)
+GTV_top_features <- Data_GTV[GTV_top_features_name]
+corMatrix_top_GTV <-  cor(GTV_top_features, y = NULL, use = "ev",method = "spearman")
+highly_correlated_columns_top_GTV <- findCorrelation(corMatrix_top_GTV,cutoff = 0.8,verbose = FALSE,names = FALSE,exact = TRUE)
+#GTV_top_features <- GTV_top_features[, -highly_correlated_columns_top_GTV]
+
+#--Brouta
+libarary("Boruta")
+set.seed(1234)
+boruta_GTV <-Boruta (GTV_top_features,as.factor(TT_Outcome),pValue = 0.01,mcAdj = TRUE,maxRuns = 500,doTrace = 2,ntree =500,holdHistory = TRUE,getImp = getImpExtraZ)
+plot(boruta_GTV)
+attStats(boruta_GTV
