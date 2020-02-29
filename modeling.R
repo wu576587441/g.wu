b1c56e62aa6719118b6c429d404c0b8a755cf00c
wu576587441 <56558856+wu576587441@users.noreply.github.com>
2020-02-29 20:00:48 +0100

    Update modeling.R

--- modeling.R
+++ modeling.R
@@ -5,13 +5,25 @@ set.seed(1234)
 RandomForest_GTV_feature <- randomForest(y = as.factor(Outcome_balanced_GTV), x = as.data.frame(data_balanced_GTV[,-4]),ntree = 500, na.action = na.exclude)
 plot(RandomForest_GTV_feature)
 varImpPlot(RandomForest_GTV_feature)
-ForestPrediction_GTV_feature<-predict(RandomForest_GTV_feature,type = "prob")[,2]
-ForestRocGTV_feature<-roc(Outcome_balanced_GTV ,ForestPrediction_GTV_feature, ci=TRUE)
+ForestPrediction_GTV_feature <-predict(RandomForest_GTV_feature,type = "prob")[,2]
+ForestRocGTV_feature <-roc(Outcome_balanced_GTV ,ForestPrediction_GTV_feature, ci=TRUE)
 ForestRocGTV_feature
 plot(ForestRocGTV_feature)
-P_GTV_training<-predict(RandomForest_GTV_feature)
-CM_GTV_training<-confusionMatrix(as.factor(P_GTV_training),as.factor(Outcome_balanced_GTV),positive = "1")
+P_GTV_training <-predict(RandomForest_GTV_feature)
+CM_GTV_training <-confusionMatrix(as.factor(P_GTV_training),as.factor(Outcome_balanced_GTV),positive = "1")
 CM_GTV_training
+
+#--testing
+set.seed(1234)
+GTV_validation <- read.table(data_validation)
+Data_GTV_validation <- GTV_validation[,3:1234]
+ForestPrediction_GTV_validation<-predict(RandomForest_GTV_feature,Data_GTV_validation,type = "prob")[,2]
+ForestRocGTV_validation<-roc(V_Outcome,ForestPrediction_GTV_validation,ci=TRUE)
+ForestRocGTV_validation
+plot(ForestRocGTV_validation)
+P_GTV_validation<-predict(RandomForest_GTV_feature,Data_GTV_validation)
+CM_GTV_validation<-confusionMatrix(as.factor(P_GTV_validation),as.factor(V_Outcome),positive = "1")
+CM_GTV_validation
 
 #---IDI
 library(PredictABEL)
